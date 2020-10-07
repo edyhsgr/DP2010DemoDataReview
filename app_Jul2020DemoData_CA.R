514c3046e0193bf6294a93abc9b491fd430c1380
Eddie Hunsinger <43016799+edyhsgr@users.noreply.github.com>
2020-10-07 11:32:09 -0700

    Update app_Jul2020DemoData_CA.R

--- app_Jul2020DemoData_CA.R
+++ app_Jul2020DemoData_CA.R
@@ -5,7 +5,7 @@
 #THIS IS UPDATE OF https://shiny.demog.berkeley.edu/DP2010DemoData_CA_Oct2019/
 #
 #EDDIE HUNSINGER, JULY 2020
-#https://edyhsgr.github.io/eddieh/
+#https://edyhsgr.github.io/
 #
 #THERE IS NO WARRANTY FOR THIS CODE
 #THIS CODE HAS NOT BEEN TESTED AT ALL-- PLEASE LET ME KNOW IF YOU FIND ANY PROBLEMS (edyhsgr@gmail.com)
